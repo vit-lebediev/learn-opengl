428c0e84a848037808f27ee56a8ab52bb7806926
Vitalii Lebediev <malgin05@gmail.com>
2018-04-24 03:18:56 -0700

    Make triangle red and flashing

--- code_snippets/triangle.cpp
+++ code_snippets/triangle.cpp
@@ -3,6 +3,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <cmath>
 
 const char* vertexShaderSource = R"glsl(
     #version 150 core
@@ -17,10 +18,12 @@ const char* vertexShaderSource = R"glsl(
 const char* fragmentShaderSource = R"glsl(
     #version 150 core
 
+    uniform vec3 triangleColor;
+
     out vec4 outColor;
 
     void main() {
-        outColor = vec4(1.0, 1.0, 1.0, 1.0);
+        outColor = vec4(triangleColor, 1.0);
     }
 )glsl";
 
@@ -131,13 +134,18 @@ int main () {
 
     glEnableVertexAttribArray(posAttrib);
 
-    
+    GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+    // glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
 
-    
-
+    auto t_start = std::chrono::high_resolution_clock::now();
     while (!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        auto t_now = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+        glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
 

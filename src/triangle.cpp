dfb9d8b66a918d52a4cfaa69075281c890dd330a
Vitalii Lebediev <malgin05@gmail.com>
2018-04-24 23:46:47 -0700

    Fix indentations.

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,11 +1,12 @@
 #define GLEW_STATIC
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <cmath>
 
-const char* vertexShaderSource = R"glsl(
+const char *vertexShaderSource = R"glsl(
     #version 150 core
 
     in vec2 position;
@@ -21,7 +22,7 @@ const char* vertexShaderSource = R"glsl(
     }
 )glsl";
 
-const char* fragmentShaderSource = R"glsl(
+const char *fragmentShaderSource = R"glsl(
     #version 150 core
 
     in float Color;
@@ -36,168 +37,169 @@ const char* fragmentShaderSource = R"glsl(
 )glsl";
 
 int main () {
-    glfwInit();
+  glfwInit();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
 
-    glfwMakeContextCurrent(window);
-    
-    glewExperimental = GL_TRUE;
-    glewInit();
+  glfwMakeContextCurrent(window);
 
-    GLuint vao; // Vertex Array Object
-    glGenVertexArrays(1, &vao);
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-    // As soon as you've bound a certain VAO, every time you call 
-    // glVertexAttribPointer, that information will be stored in that VAO.
-    //
-    // Since only calls after binding a VAO stick to it, make sure 
-    // that you've created and bound the VAO at the start of your 
-    // program. Any vertex buffers and element buffers bound 
-    // before it will be ignored.
-    glBindVertexArray(vao);
+  GLuint vao; // Vertex Array Object
+  glGenVertexArrays(1, &vao);
 
-    // Defining vertex data
-    /*
-    float vertices[] = {
-         0.0f,  0.5f, 1.0f, 0.0f, 0.0f, // Vertex 1: Red
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Vertex 2: Green
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f  // Vertex 3: Blue
-    };
-    */
+  // As soon as you've bound a certain VAO, every time you call
+  // glVertexAttribPointer, that information will be stored in that VAO.
+  //
+  // Since only calls after binding a VAO stick to it, make sure
+  // that you've created and bound the VAO at the start of your
+  // program. Any vertex buffers and element buffers bound
+  // before it will be ignored.
+  glBindVertexArray(vao);
 
-    float vertices[] = {
-         0.0f,  0.5f, 0.2f, // Vertex 1: Shades of Gray
-         0.5f, -0.5f, 0.7f, // Vertex 2: Shades of Gray
-        -0.5f, -0.5f, 0.3f  // Vertex 3: Shades of Gray
-    };
+  // Defining vertex data
+  /*
+  float vertices[] = {
+       0.0f,  0.5f, 1.0f, 0.0f, 0.0f, // Vertex 1: Red
+       0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Vertex 2: Green
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f  // Vertex 3: Blue
+  };
+  */
 
-    /*
-    float vertices[] = {
-        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right    
-         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, // Bottom-left
-    };
-    */
+  float vertices[] = {
+      0.0f, 0.5f, 0.2f, // Vertex 1: Shades of Gray
+      0.5f, -0.5f, 0.7f, // Vertex 2: Shades of Gray
+      -0.5f, -0.5f, 0.3f  // Vertex 3: Shades of Gray
+  };
 
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+  /*
+  float vertices[] = {
+      -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+       0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+       0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+      -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, // Bottom-left
+  };
+  */
 
-    GLuint vbo; // Vertex Buffer Object
-    glGenBuffers(1, &vbo); // Generate 1 buffer
+  GLuint elements[] = {
+      0, 1, 2,
+      2, 3, 0
+  };
 
-    // Bind Vertex Buffer Object 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // Load vertex data to binded buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  GLuint vbo; // Vertex Buffer Object
+  glGenBuffers(1, &vbo); // Generate 1 buffer
 
-    // GLuint ebo; // Element Buffer Object
-    // glGenBuffers(1, &ebo);
+  // Bind Vertex Buffer Object
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  // Load vertex data to binded buffer
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+  // GLuint ebo; // Element Buffer Object
+  // glGenBuffers(1, &ebo);
+
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
 
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
 
-    GLint status;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE) {
-        // Shader failed to compile, retrieving log...
-        char buffer[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-        std::cout << "Vertex Shader Issues!" << std::endl; 
-        std::cout << buffer << std::endl;
-    }
+  GLint status;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    // Shader failed to compile, retrieving log...
+    char buffer[512];
+    glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+    std::cout << "Vertex Shader Issues!" << std::endl;
+    std::cout << buffer << std::endl;
+  }
 
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE) {
-        // Shader failed to compile, retrieving log...
-        char buffer[512];
-        glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
-        std::cout << "Fragment Shader Issues!" << std::endl; 
-        std::cout << buffer << std::endl;
-    }
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    // Shader failed to compile, retrieving log...
+    char buffer[512];
+    glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
+    std::cout << "Fragment Shader Issues!" << std::endl;
+    std::cout << buffer << std::endl;
+  }
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
 
-    // Since a fragment shader is allowed to write to multiple buffers, 
-    // you need to explicitly specify which output is written to which buffer. 
-    // This needs to happen before linking the program. However, since this is 
-    // 0 by default and there's only one output right now, the following line
-    // of code is not necessary:
-    glBindFragDataLocation(shaderProgram, 0, "outColor"); // Use glDrawBuffers when rendering to multiple buffers, because only the first output will be enabled by default.
+  // Since a fragment shader is allowed to write to multiple buffers,
+  // you need to explicitly specify which output is written to which buffer.
+  // This needs to happen before linking the program. However, since this is
+  // 0 by default and there's only one output right now, the following line
+  // of code is not necessary:
+  glBindFragDataLocation(shaderProgram, 0,
+                         "outColor"); // Use glDrawBuffers when rendering to multiple buffers, because only the first output will be enabled by default.
 
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram); // Just like a vertex buffer, only one program can be active at a time.
-    
-    // A shader object can be deleted with glDeleteShader, but it will not 
-    // actually be removed before it has been detached from all programs 
-    // with glDetachShader
-    
-    // The location is a number depending on the order of the input definitions. 
-    // The first and only input position in this example will always have location 0.
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram); // Just like a vertex buffer, only one program can be active at a time.
 
-    // This function also stores VBO currently bound to GL_ARRAY_BUFFER.
-    // This means that with next invocation we can bind different buffer
-    glVertexAttribPointer(
-        posAttrib, // reference to input
-        2,         // number of values for input (number of components of vec) 
-        GL_FLOAT,  // type of each component
-        GL_FALSE,  // whether imput values should be normalized between -1.0 and 1.0
-        3 * sizeof(float), // stride (0 - no data between data attributes)
-        0                  // offset (how many bytes from the start of the array the attributes occur)
+  // A shader object can be deleted with glDeleteShader, but it will not
+  // actually be removed before it has been detached from all programs
+  // with glDetachShader
+
+  // The location is a number depending on the order of the input definitions.
+  // The first and only input position in this example will always have location 0.
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+
+  // This function also stores VBO currently bound to GL_ARRAY_BUFFER.
+  // This means that with next invocation we can bind different buffer
+  glVertexAttribPointer(
+      posAttrib, // reference to input
+      2,         // number of values for input (number of components of vec)
+      GL_FLOAT,  // type of each component
+      GL_FALSE,  // whether imput values should be normalized between -1.0 and 1.0
+      3 * sizeof(float), // stride (0 - no data between data attributes)
+      0                  // offset (how many bytes from the start of the array the attributes occur)
+  );
+
+  GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+  glEnableVertexAttribArray(colAttrib);
+  glVertexAttribPointer(colAttrib, 1, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) (2 * sizeof(float)));
+
+  GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+  // glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
+
+  auto t_start = std::chrono::high_resolution_clock::now();
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    auto t_now = std::chrono::high_resolution_clock::now();
+    float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+    // glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
+
+    glDrawArrays(
+        GL_TRIANGLES, // primitive (commonly point, line or triangle)
+        0,            // how many vertices to skip at the beginning
+        3             // number of vertices (not primitives!) to process
     );
 
-    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
-    glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 1, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(2 * sizeof(float)));
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
-    // glUniform3f(uniColor, 1.0f, 0.0f, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
+  }
 
-    auto t_start = std::chrono::high_resolution_clock::now();
-    while (!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+  glfwTerminate();
 
-        auto t_now = std::chrono::high_resolution_clock::now();
-        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
-
-        // glUniform3f(uniColor, (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
-
-        glDrawArrays(
-            GL_TRIANGLES, // primitive (commonly point, line or triangle)
-            0,            // how many vertices to skip at the beginning
-            3             // number of vertices (not primitives!) to process
-        );
-
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-
-    glfwTerminate();
-
-    return 0;
+  return 0;
 }
